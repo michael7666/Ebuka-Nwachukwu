58e6bc975a1b3cbc003bb8c5393efbe0c78858b4
Ebuka Nwachukwu <ebuka.nwachukw885@signalalliance.com>
2020-06-03 16:45:07 +0100

    ebuka-HNG-00968.js

---
+++ Desktop/HNGi7-task-2-develop/scripts/ebuka-HNG-00968.js
@@ -0,0 +1,15 @@
+var myName = "Ebuka Nwachukwu";
+var hngID = "HNG-00968";
+var email = "ebukanwachukwu885@gmail.com";
+var lang = "javascript";
+console.log(
+  "hello word this is ! " +
+    myName +
+    " with HNGi7 ID  " +
+    hngID +
+    " and " +
+    email +
+    " using " +
+    lang +
+    " for task 2 "
+);
